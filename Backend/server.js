1685c9d5b41bf2e986951a6345cf03c25ea56436
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2024-12-25 19:36:27 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,13 @@ import userRoute from './Route/user.route.js'
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+  origin: ["https://bookstore-mern.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+  }
+));
 app.use(express.json());
 
 const port = 3000
